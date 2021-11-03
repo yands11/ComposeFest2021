8ad8bbbff06ebe104876053236959d2d2128b877
dot2line <yands11@gmail.com>
2021-11-03 21:13:10 +0900

    [Week 1] 12 Styling and theming your app

--- week 1-Jetpack Compose basics/Week1Codelab/app/src/main/java/com/codelab/week1codelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/Week1Codelab/app/src/main/java/com/codelab/week1codelab/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.codelab.week1codelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -26,6 +27,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.week1codelab.ui.theme.Week1CodelabTheme
@@ -106,7 +108,12 @@ private fun Greeting(name: String) {
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.h4.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
             OutlinedButton(
                 onClick = { expanded = !expanded }
@@ -125,7 +132,13 @@ fun OnboardingPreview() {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     Week1CodelabTheme {
